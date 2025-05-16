31a2edca15fcb4c594fcbda47fbb90bc5c0574db
jkooij <john.van.der.kooij@enexis.nl>
2025-05-16 21:13:48 +0200

    deploying sql files via snowsql

--- sql/t1.sql
+++ sql/t1.sql
@@ -1,2 +1,5 @@
-create table d.dev.s_Dev.t_Dev.t1
+create or replace table d.dev.s_Dev.t_Dev.t1
 (description varchar(100)
+, 
+  created_at timestamp default current_timestamp, 
+  updated_at timestamp default current_timestamp on update current_timestamp)
